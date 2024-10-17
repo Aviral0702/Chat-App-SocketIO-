05dd17d12c60f5575b49b677a35870cf70602f42
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-10-17 11:11:02 +0530

    Completed the chat application

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const http = require('http');
+const {Server} = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 app.use(express.static("./public"));
+
+const io = new Server(server);
+
+io.on('connection',(socket)=>{
+    socket.on('chat message',(msg)=>{
+        io.emit('chat message',msg);
+    })
+})
+
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html");
 })
